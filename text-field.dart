a7244a478e1c3bdae9dd0dc11bb5b06071fdc132
Deepa Pandey <deepapandey364@gmail.com>
2021-05-07 14:43:58 +0530

    Create text-field.dart

---
+++ text-field.dart
@@ -0,0 +1,7 @@
+
+//Multi-line Textfield in flutter
+TextField(
+    keyboardType: TextInputType.multiline,
+    minLines: 1,//Normal textInputField will be displayed
+    maxLines: 5,// when user presses enter it will adapt to it
+    );
