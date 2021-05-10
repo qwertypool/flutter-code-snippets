1cbcb7b9287a9989d66f7a7dfe34d8faad9120a2
Deepa Pandey <deepapandey364@gmail.com>
2021-05-10 16:53:54 +0530

    Update adding-svg-icons.dart

--- adding-svg-icons.dart
+++ adding-svg-icons.dart
@@ -1,13 +1,17 @@
-#firstly add the  flutter_svg: ^0.22.0 (whichever be the latest version) in your pubspec.yaml file
+//firstly add the dependencies
+flutter_svg: ^0.22.0    //(whichever be the latest version) in your pubspec.yaml file
 
- #then import the package - 
+ // then import the package - 
  import 'package:flutter_svg/svg.dart';
 
-#then add the svg icon like : 
-              SvgPicture.asset(
-                             "assets/icons/chat.svg",
-                           // color: Theme.of(context).textTheme.bodyText1!.color, (for light/dark theme compatiblity )
-                            color: Colors.red,
-                            height: 200,
-                            width: 200,
-                          ),
+// then add the svg icon like : 
+SvgPicture.asset(
+               "assets/icons/chat.svg",
+             // color: Theme.of(context).textTheme.bodyText1!.color, (for light/dark theme compatiblity )
+              color: Colors.red,
+              height: 200,
+              width: 200,
+            ),
+
+//Preffered site for best svgs (Personal recommendation) -
+https://iconscout.com/icons/
