d8121e0c169276e017b633ed445f483a44f8649e
Deepa Pandey <deepapandey364@gmail.com>
2021-05-10 15:11:45 +0530

    Create search-delegate.dart

---
+++ search-delegate.dart
@@ -0,0 +1,95 @@
+
+//Implementing search functionality using search delegate in flutter.
+
+class TextBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextField(
+        cursorHeight: 20,
+        decoration: InputDecoration(
+          hintText: 'Search in mail',
+          border: InputBorder.none,
+        ),
+        onTap: () {
+          showSearch(context: context, delegate: Datasearch());
+        },
+      ),
+    );
+  }
+}
+
+class Datasearch extends SearchDelegate<String> {
+  final names = [
+    'deepa',
+    'deepak',
+    'sugarcosmetics',
+    'balram',
+    'linkedln',
+    'banglore',
+    'balram0698@gmail.com',
+    'pandey.deepa@tcs.com',
+    'sugarcosmetics@gmail.com',
+    'pandeydeepak821@gmail.com',
+    'balram.rathore@tcs.com',
+  ];
+  final recentSearches = [
+    'pandey.deepa@tcs.com',
+    'sugarcosmetics@gmail.com',
+    'pandeydeepak821@gmail.com',
+    'balram0698@gmail.com'
+  ];
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      
+      IconButton(
+        icon: Icon(Icons.mic),
+        onPressed: () {},
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentSearches
+        : names.where((element) => element.startsWith(query)).toList();
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.person_search),
+        title: RichText(
+            text: TextSpan(
+                text: suggestionList[index].substring(0, query.length),
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                children: [
+              TextSpan(
+                  text: suggestionList[index].substring(query.length),
+                  style: TextStyle(color: Colors.grey))
+            ])),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
+
+
