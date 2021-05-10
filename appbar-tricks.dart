e3de601248d22358779e4d5587dffa0f4bc6ca20
Deepa Pandey <deepapandey364@gmail.com>
2021-05-10 16:50:58 +0530

    Update appbar-tricks.dart

--- appbar-tricks.dart
+++ appbar-tricks.dart
@@ -1,7 +1,33 @@
-//Removing extra padding around appbar leading icon -  
+//Removing extra padding around appbar leading icon(use titlespacing) -  
 
 appBar: AppBar(
         leading: Icon(Icons.android),
         titleSpacing: 0, //Just add a property called titleSpacing,
         title: Text(widget.title),
       ),
+
+// To increse the leading width of appbar - 
+
+appBar: AppBar(
+  leading: Icon(Icons.account_circle_rounded),
+  leadingWidth: 100, // default is 56
+),
+
+//To increase the default height & alter opacity of appbar - 
+
+appBar: AppBar(
+  toolbarHeight: 120,
+  toolbarOpacity: 0.5,
+  leadingWidth: 100, // default is 56
+),
+
+// Image in title in appbar - 
+
+AppBar(
+  title: Container(
+    width: 40,
+    child: Image.network(url),
+  ),
+  centerTitle: true, 
+),
+
