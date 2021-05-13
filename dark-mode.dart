4aa1484ac50815021894e5529ad9c08a399eb2ac
Deepa Pandey <deepapandey364@gmail.com>
2021-05-13 15:26:06 +0530

    update

---
+++ dark-mode.dart
@@ -0,0 +1,36 @@
+//set the color to -
+
+color: Theme.of(context).textTheme.bodyText1!.color
+
+
+//checking if the system is in dark mode or light mode - 
+Image.asset(
+      MediaQuery.of(context).platformBrightness == Brightness.light
+        ? "assets/lightmode2.png"
+        :"assets/darkmode.png",
+         height: 256,
+          )
+
+      
+//Check system mode  -
+       Widget build(BuildContext context) {
+       final Brightness brightnessValue = MediaQuery.of(context).platformBrightness;
+       bool isDark = brightnessValue == Brightness.dark;
+       . . . . . 
+       . . . . .
+       . . . . .
+       Text(
+          'Choose your theme',
+          style:  isDark == true ? TextStyle(
+             color: Colors.black,
+              fontSize: 16,
+             letterSpacing: 4,
+             fontWeight: FontWeight.w500
+             ):
+             TextStyle(
+             color: Colors.white,
+             fontSize: 16,
+             letterSpacing: 4,
+             fontWeight: FontWeight.w500
+             ),
+                ),
