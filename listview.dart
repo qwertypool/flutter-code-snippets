3b5d667deb981b542766b85aab5a4f05ee00e2eb
Deepa Pandey <deepapandey364@gmail.com>
2021-05-17 14:21:39 +0530

    Create listview.dart

---
+++ listview.dart
@@ -0,0 +1,18 @@
+// To create a list of items using listview ---
+
+
+ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, position) {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Your data',
+                  style: TextStyle(fontSize: 22.0),
+                ),
+              ),
+            );
+          },
+        ),
+
