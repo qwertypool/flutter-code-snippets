72611c727bf69c70e0d7631b6b47557245daec82
Deepa Pandey <deepapandey364@gmail.com>
2021-05-20 10:30:25 +0530

    Update bottom-nav-bar.dart

--- bottom-nav-bar.dart
+++ bottom-nav-bar.dart
@@ -32,3 +32,52 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 }
+
+
+-----------------------------/\/\/\/\/\/\\/\/\/\/\/\/\/\\/\/\/\/\/\/\----------------------------------------------------------
+  
+  //Bottom navbar with circle avartar + change of icons when moved from one page to another
+  
+  Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedIconTheme: IconThemeData(color: pPrimaryColor),
+        currentIndex: _selectedIndex,
+        backgroundColor: !isDark?Colors.white:Colors.black,
+        selectedItemColor: isDark?Colors.grey:Colors.black,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        onTap: (value) {
+          setState(() {
+            _selectedIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: _selectedIndex==0?Icon(Icons.home,color: Colors.black,size: 28,): Icon(Icons.home_outlined,color: Colors.black,size: 28,),label: 'home'),
+          BottomNavigationBarItem(icon:  _selectedIndex==1?Icon(Icons.search,color: Colors.black,size: 28,): Icon(Icons.search_outlined,color: Colors.black,size: 28,),label: 'search'),
+          BottomNavigationBarItem(icon:  _selectedIndex==2?Icon(Icons.video_collection_sharp,color: Colors.black,size: 28,): Icon(Icons.video_collection_outlined,color: Colors.black,size: 28,),label: 'reels' ),
+          BottomNavigationBarItem(icon: _selectedIndex==3?Icon(Icons.favorite,color: Colors.black,size: 28,):Icon(Icons.favorite_border_outlined,color: Colors.black,size: 28,),label: 'notif'),
+          BottomNavigationBarItem(icon: _selectedIndex==4?
+           CircleAvatar(
+             radius: 14,
+             backgroundColor: Colors.black,
+              child: CircleAvatar(
+               radius: 13,
+               backgroundImage:AssetImage('assets/profilepic.jpg') ,),
+           ):
+              CircleAvatar(
+             radius: 14,
+             backgroundImage:AssetImage('assets/profilepic.jpg') ,),
+             label: 'profile'
+           ),
+        
+        ],
+        
+      ),
+     );
+  
+  
+  
