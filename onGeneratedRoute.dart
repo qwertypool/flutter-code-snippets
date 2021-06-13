e3a20f444d3568cfe059445e870bff48b138b1bf
Lokesh Jangid <64513385+lkrjangid1@users.noreply.github.com>
2021-06-13 18:36:43 +0530

    Create onGeneratedRoute.dart

---
+++ onGeneratedRoute.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_concepts/presentation/screens/home_screen.dart';
+import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
+import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_concepts/presentation/router/app_router.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter _appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
+
+
+class AppRouter {
+  Route onGenerateRoute(RouteSettings settings) {
+    final GlobalKey<ScaffoldState> key = settings.arguments;
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => HomeScreen(
+            title: "Home Screen",
+            color: Colors.blueAccent,
+          ),
+        );
+      case '/second':
+        return MaterialPageRoute(
+          builder: (_) => SecondScreen(
+            title: "Second Screen",
+            color: Colors.redAccent,
+            homeScreenKey: key,
+          ),
+        );
+      case '/third':
+        return MaterialPageRoute(
+          builder: (_) => ThirdScreen(
+            title: "Thirst Screen",
+            color: Colors.greenAccent,
+          ),
+        );
+      default:
+        return null;
+    }
+  }
+}
