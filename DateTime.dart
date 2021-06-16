fa8dd6750a6414888c5fa2d0c729d5a05ce06b85
Deepa Pandey <deepapandey364@gmail.com>
2021-06-16 23:43:35 +0530

    Create DateTime.dart

---
+++ DateTime.dart
@@ -0,0 +1,18 @@
+DateTimeFormField(
+                          
+    mode: DateTimeFieldPickerMode.date,
+    decoration: const InputDecoration(
+      hintStyle: TextStyle(color: Colors.black45),
+      errorStyle: TextStyle(color: Colors.redAccent),
+      border: OutlineInputBorder(),
+      suffixIcon: Icon(Icons.event_note,color: Colors.black,),
+      labelStyle:TextStyle(fontSize: 20,color: Colors.grey,letterSpacing: 1.2),
+      labelText: 'Date of birth',
+    ),
+    // autovalidateMode: AutovalidateMode.always,
+    // validator: (e) =>
+    //     (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+    onDateSelected: (DateTime value) {
+      print(value);
+    },
+ ),
