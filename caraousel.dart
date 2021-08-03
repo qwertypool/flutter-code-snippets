cac6b5b9d9d0889a17b1db4f8c2009add8e0aa5c
Deepa Pandey <deepapandey364@gmail.com>
2021-08-03 20:01:47 +0530

    Create caraousel.dart

---
+++ caraousel.dart
@@ -0,0 +1,106 @@
+class ImageSliderDemo extends StatefulWidget {
+  @override
+  _ImageSliderDemoState createState() => _ImageSliderDemoState();
+}
+ int _current = 0;
+ final CarouselController _controller = CarouselController();
+class _ImageSliderDemoState extends State<ImageSliderDemo> {
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1610438235354-a6ae5528385c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+    'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80'
+    'https://images.unsplash.com/photo-1606220588913-b3aacb4d2f46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
+    'https://images.unsplash.com/photo-1585298723682-7115561c51b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
+    'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1569429594806-192f16855a0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1548484352-ea579e5233a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+    'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80'
+  ];
+  
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Column(
+      children: [
+        Container(
+            child: CarouselSlider(
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            enableInfiniteScroll: true,
+            onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }
+          ),
+          items: imgList
+              .map((item) => Container(
+                    child: Center(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: 1200,
+                              decoration:BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                image: DecorationImage(
+                                  image: NetworkImage(item),
+                                  fit: BoxFit.cover,)),
+                                  ),
+                              //: Image.network(item, fit: BoxFit.cover, width: 1200)),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 10,bottom: 6),
+                                child: ClipRect(
+
+                                  child: BackdropFilter(
+                                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                                    child: Container(
+                                      width: 120,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.grey.withOpacity(0.5),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text('Shop Now',style: TextStyle(color: Colors.white,fontFamily: 'Ubuntu',letterSpacing: 2,fontWeight: FontWeight.w600),),
+                                        Icon(Icons.arrow_forward_ios,color: Colors.white,size: 15,),
+                                      ],
+                                    ),
+                                    ),
+                                    
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        )),
+                  ))
+              .toList(),
+        )),
+              Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: imgList.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () => _controller.animateToPage(entry.key),
+            child: Container(
+              width: 6.0,
+              height: 6.0,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: (Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black)
+                      .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+            ),
+          );
+        }).toList(),
+      ),
+      ],
+
+    );
+  }
+}
