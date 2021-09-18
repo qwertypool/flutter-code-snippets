eed598955ca9b3402a04b123ccae28e31d2a7092
Deepa Pandey <deepapandey364@gmail.com>
2021-09-18 20:58:45 +0530

    Create refresh_indicator.dart

---
+++ refresh_indicator.dart
@@ -0,0 +1,142 @@
+//Flutter Custom Refresh Indicator 
+// This package provides CustomRefreshIndicator widget that make it easy to implement your own custom refresh indicator. 
+//It listens for scroll events from scroll widget passed to child argument and parsing it to data easy for custom refresh indicator implementation.
+//Indicator data is provided by IndicatorController (third argument of builder method). Long story short... thats it!
+
+
+import 'package:example/screens/presentation_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'indicators/simple_indicator.dart';
+import 'screens/example_indicator_screen.dart';
+import 'screens/ice_cream_indicator_screen.dart';
+import 'screens/plane_indicator_screen.dart';
+import 'screens/check_mark_indicator_screen.dart';
+import 'screens/warp_indicator_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CustomRefreshIndicator demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+      routes: {
+        '/example': (context) => ExampleIndicatorScreen(),
+        '/plane': (context) => PlaneIndicatorScreen(),
+        '/ice_cream': (context) => IceCreamIndicatorScreen(),
+        '/presentation': (context) => PresentationScreen(),
+        '/check-mark': (context) => CheckMarkIndicatorScreen(),
+        '/warp': (context) => WarpIndicatorScreen(),
+      },
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Examples"),
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.all(15),
+          children: <Widget>[
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Presentation"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/presentation',
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Simple"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/example',
+                arguments: simpleIndicator,
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Simple with list opacity"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/example',
+                arguments: simpleIndicatorWithOpacity,
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Plane"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/plane',
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Ice cream"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/ice_cream',
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Check mark"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/check-mark',
+              ),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: Container(
+                height: 50,
+                alignment: Alignment.center,
+                child: Text("Warp indicator"),
+              ),
+              onPressed: () => Navigator.pushNamed(
+                context,
+                '/warp',
+                arguments: simpleIndicator,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
