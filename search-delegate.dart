b022d36623597f152bd98f54006717831cf6c1e4
Deepa Pandey <deepapandey364@gmail.com>
2021-12-17 22:40:15 +0530

    Update search-delegate.dart

--- search-delegate.dart
+++ search-delegate.dart
@@ -1,22 +1,4 @@
 
-//Implementing search functionality using search delegate in flutter.
-class TextBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: TextField(
-        cursorHeight: 20,
-        decoration: InputDecoration(
-          hintText: 'Search in mail',
-          border: InputBorder.none,
-        ),
-        onTap: () {
-          showSearch(context: context, delegate: Datasearch());
-        },
-      ),
-    );
-  }
-}
 
 class Datasearch extends SearchDelegate<String> {
   final names = [
