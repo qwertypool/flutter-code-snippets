279ef0cab7eb4fc17b576a45995368431bf5ab67
Deepa Pandey <deepapandey364@gmail.com>
2021-12-25 22:50:13 +0530

    Update dropdown.dart

--- dropdown.dart
+++ dropdown.dart
@@ -1,2 +1,32 @@
 //demo dropdown ->
 
+List<String> _items = [
+    'Accounts',
+    'Management',
+    'Tech',
+    'Product',
+    'Design',
+    'Operations'
+  ];
+  Widget drop() {
+    return DropdownButtonHideUnderline(
+      child: new DropdownButton(
+        isExpanded: true,
+        hint: Text('Department'),
+       value: value,
+        style: TextStyle(fontSize: 18, color: Colors.black),
+        items: _items.map((value) {
+          return new DropdownMenuItem(
+            value: value,
+            child: new Text(value),
+          );
+        }).toList(),
+        onChanged: (newValue) {
+          setState(() {
+            value = newValue;
+          });
+          print(value);
+        },
+      ),
+    );
+  }
