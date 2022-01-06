86b88c7499d3eaae2a976bcac9f412ee811635ae
Deepa Pandey <deepapandey364@gmail.com>
2022-01-06 22:09:57 +0530

    Update test.dart

--- test.dart
+++ test.dart
@@ -1,1 +1,13 @@
 
+Container(
+      height: 120.0,
+      width: 120.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/assets/alucard.jpg'),
+          fit: BoxFit.fill,
+        ),
+        shape: BoxShape.circle,
+      ),
+    )
